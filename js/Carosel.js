0588b03dd22f23174422d966b37791fbfb3f97f5
Diogo Gomes <diogo.ferreira@uniara.edu.br>
2024-09-11 21:34:22 -0300

    Carosel-2

--- js/Carosel.js
+++ js/Carosel.js
@@ -13,14 +13,23 @@ function showDivs(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("demo");
+
     if (n > slides.length) { slideIndex = 1; }
     if (n < 1) { slideIndex = slides.length; }
+
+    // Oculta todas as slides
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+
+    // Remove a classe 'w3-badge-active' de todos os badges
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" w3-badge", "");
+        dots[i].classList.remove("w3-badge-active");
     }
+
+    // Mostra a slide atual
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " w3-badge";
+
+    // Adiciona a classe 'w3-badge-active' ao badge correspondente
+    dots[slideIndex-1].classList.add("w3-badge-active");
 }
